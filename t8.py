2fe7538f22a9358a4d17cd40fe988c75f4b03152
Игнатий Мягков <102025412+Myggis@users.noreply.github.com>
2022-05-14 18:54:47 +0300

    'l8'

---
+++ t8.py
@@ -0,0 +1,18 @@
+e = '12-12-2022'
+x = ''
+y = ''
+z = ''
+
+
+# my_list = e.split('-')
+# # print(my_list)
+#
+# for el in range(0, len(my_list)):
+#     print(el, my_list[el], end="\n")
+
+# x = my_list[0]
+# y = my_list[1]
+# z = my_list[2]
+print(e.split('-')[0])
+print(e.split('-')[1])
+print(e.split('-')[2])
